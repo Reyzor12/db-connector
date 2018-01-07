933653cace74f5d57973d53862a53a8dfd837acb
Reyzor <testaccwow001@gmail.com>
2018-01-07 21:01:53 +0300

    db create ok

--- src/main/java/ru/eleron/osa/lris/dbconnector/StartApp.java
+++ src/main/java/ru/eleron/osa/lris/dbconnector/StartApp.java
@@ -6,7 +6,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.eleron.osa.lris.dbconnector.utii.SceneLoader;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class StartApp extends Application {
+
+    public static Connection connection;
 
     public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext("configSpring.xml");
@@ -14,10 +20,24 @@ public class StartApp extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
+        Class.forName("org.h2.Driver");
+        connection = DriverManager.getConnection("jdbc:h2:./test","test","");
+
         primaryStage.setMinHeight(270);
         primaryStage.setMinWidth(370);
 //        primaryStage.setResizable(false);
         SceneLoader.setStage(primaryStage);
         SceneLoader.loadScene("view/MainFrame.fxml");
     }
+
+    public void stop(){
+        try {
+            if(!connection.isClosed()){
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
