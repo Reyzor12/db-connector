209581de51e0cd65c99d86444f3df4d79b9eda63
Reyzor12 <testaccwow001@gmail.com>
2018-01-10 15:24:26 +0300

    finally work!

--- src/main/java/ru/eleron/osa/lris/dbconnector/controller/MainFrameController.java
+++ src/main/java/ru/eleron/osa/lris/dbconnector/controller/MainFrameController.java
@@ -1,6 +1,12 @@
 package ru.eleron.osa.lris.dbconnector.controller;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
@@ -17,19 +23,27 @@ import ru.eleron.osa.lris.dbconnector.utii.SceneLoader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class MainFrameController {
 
-    private Image findDB = new Image("image/findDB.png");
-    private Image notFindDB = new Image("image/notFindDB.png");
+    private IntegerProperty intValue = new SimpleIntegerProperty();
 
     public static ConnectorDB connectorDB;
 
     private Stage stage;
 
+    private Boolean status = false;
+
+    public void setStatus(Boolean b){
+        status = b;
+    }
+
     public Stage getStage(){return stage;}
+
+    public List<Image> imageList = Arrays.asList(new Image("image/1.png"), new Image("image/2.png"), new Image("image/3.png"), new Image("image/4.png"),new Image("image/findDB.png"),new Image("image/notFindDB.png"));
 
     @Autowired
     private H2DAO h2DAO;
@@ -51,7 +65,10 @@ public class MainFrameController {
     private List<ConnectorDB> listConnectorDB;
 
     public void initialize(){
-       updateData();
+
+        updateData();
+        imageViewCheckConnection.imageProperty().bind(Bindings.createObjectBinding(()->
+            imageList.get(intValue.getValue()),intValue));
     }
 
     @FXML
@@ -86,10 +103,8 @@ public class MainFrameController {
         addConnection();
     }
 
-    @FXML
-    public void checkConnection(){
-        if(listViewConnectorDB.getSelectionModel().getSelectedIndex()==-1) return;
-        ConnectorDB c = listViewConnectorDB.getSelectionModel().getSelectedItem();
+    public void checkConnection(ConnectorDB c){
+
         switch(c.getTypeDB()){
             case 0: {
                 String Url = "jdbc:mysql://" +
@@ -98,24 +113,19 @@ public class MainFrameController {
                 try{
                     Class.forName("com.mysql.jdbc.Driver").newInstance();
                     Connection con = DriverManager.getConnection(Url);
-                    imageViewCheckConnection.setImage(findDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(true);
                     con.close();
                 }catch (ClassNotFoundException e) {
-                    imageViewCheckConnection.setImage(notFindDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 } catch (SQLException e) {
-                    imageViewCheckConnection.setImage(notFindDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 } catch (IllegalAccessException e) {
-                    imageViewCheckConnection.setImage(notFindDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 } catch (InstantiationException e) {
-                    imageViewCheckConnection.setImage(notFindDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 }
             };break;
@@ -126,16 +136,13 @@ public class MainFrameController {
                 try{
                     Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
                     Connection con = DriverManager.getConnection(Url);
-                    imageViewCheckConnection.setImage(findDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(true);
                     con.close();
                 }catch (ClassNotFoundException e) {
-                    imageViewCheckConnection.setImage(notFindDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 } catch (SQLException e) {
-                    imageViewCheckConnection.setImage(notFindDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 }
             };break;
@@ -146,28 +153,69 @@ public class MainFrameController {
                 try{
                     Class.forName("org.postgresql.Driver").newInstance();
                     Connection con = DriverManager.getConnection(Url);
-                    imageViewCheckConnection.setImage(findDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(true);
                     con.close();
                 }catch (ClassNotFoundException e) {
-                    imageViewCheckConnection.setImage(notFindDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 } catch (SQLException e) {
-                    imageViewCheckConnection.setImage(notFindDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 } catch (IllegalAccessException e) {
-                    imageViewCheckConnection.setImage(findDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 } catch (InstantiationException e) {
-                    imageViewCheckConnection.setImage(findDB);
-                    imageViewCheckConnection.setVisible(true);
+                    setStatus(false);
                     e.printStackTrace();
                 }
             };break;
             default:break;
+        }
+    }
+
+
+    @FXML
+    public void startConnection(){
+        if(listViewConnectorDB.getSelectionModel().getSelectedIndex()==-1) return;
+        Thread t = new Thread(new CheckTask(listViewConnectorDB.getSelectionModel().getSelectedItem()));
+        t.setDaemon(true);
+        t.start();
+    }
+
+    private class CheckTask extends Task<Void>{
+
+        ConnectorDB connectorDB;
+
+        CheckTask(ConnectorDB connectorDB){
+            this.connectorDB = connectorDB;
+        }
+
+        @Override
+        protected Void call() throws Exception {
+            buttonCheck.setDisable(true);
+            imageViewCheckConnection.setVisible(true);
+            Thread t = new Thread (new Task(){
+
+                @Override
+                protected Object call() throws Exception {
+                    checkConnection(connectorDB);
+                    return null;
+                }
+            });
+            t.start();
+            int i = 0;
+            while(t.isAlive()){
+                t.join(250);
+                i = (i+1)%4;
+                intValue.set(i);
+            }
+            buttonCheck.setDisable(false);
+            if(status==true){
+                intValue.set(4);
+            } else{
+                intValue.set(5);
+            }
+            return null;
         }
     }
 }
